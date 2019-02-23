d6c5b2bd92c17b2b57bceef4c589dee6b17c85fc
Ostricker <hrazdil.jan@gmail.com>
2019-02-23 14:34:46 +0100

    CSV reader finalized, program ready to deploy

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,6 @@ public class Main extends Application {
   public void start(Stage primaryStage) throws Exception {
 
     Parent root = FXMLLoader.load(getClass().getResource("/MainWindow.fxml"));
-
     Scene scene = new Scene(root);
 
     primaryStage.setTitle("Apyho prográmek");
